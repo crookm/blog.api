df9e5b7f3359b45f971fe09ea5082390674aa1c9
Matt Crook <mcro591@aucklanduni.ac.nz>
2018-10-22 17:02:54 +1300

    add cors

--- index.js
+++ index.js
@@ -3,8 +3,16 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    origin: "*",
+    optionsSuccessStatus: 200
+  })
+);
 
 /**
  * SVCWEB
