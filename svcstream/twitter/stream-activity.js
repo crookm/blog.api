f7fe58fc35381ab25a75e40683e0758096591103
Matt Crook <mcro591@aucklanduni.ac.nz>
2018-10-23 13:42:29 +1300

    use a shared store for consistency

--- svcstream/twitter/stream-activity.js
+++ svcstream/twitter/stream-activity.js
@@ -1,33 +1,13 @@
 const { performance } = require("perf_hooks");
-const url = require('url');
+const url = require("url");
 
-const store = require("../../util/store-do");
+const tw_threads = require("../../util/store-tw_threads");
 const tw = require("twitter");
 const twclient = new tw({
   consumer_key: process.env.TWCKEY,
   consumer_secret: process.env.TWCSEC,
   access_token_key: process.env.TWATKEY,
   access_token_secret: process.env.TWATSEC
-});
-
-let thread_tweets = {};
-
-// hydrate thread tweets
-console.info(`[i] svcstream:twitter:stream-activity - hydrating threads...`);
-let boot_thread_start = performance.now();
-store.get_obj("api/obj/twitter/threads.json", (err, data) => {
-  if (err) {
-    console.error(`[*] svcstream:twitter:stream-activity - ${err}`);
-  } else {
-    let body = JSON.parse(data.Body);
-    if (body) thread_tweets = body.data;
-
-    console.info(
-      `[i] svcstream:twitter:stream-activity - finished thread hydration (${(
-        performance.now() - boot_thread_start
-      ).toFixed(2)}ms)`
-    );
-  }
 });
 
 module.exports = () => {
@@ -63,9 +43,9 @@ class EventHandler {
         console.info(
           `[i] svcstream:twitter:stream-activity - reply to ${
             event.in_reply_to_status_id_str
-          } from ${event.user.screen_name} for ${
-            thread_tweets[event.in_reply_to_status_id_str]
-          }`
+          } from ${event.user.screen_name} for ${tw_threads.get_thread(
+            event.in_reply_to_status_id_str
+          )}`
         );
       },
       () => {
@@ -77,48 +57,35 @@ class EventHandler {
   static is_thread_tweet(tw_id, cb_true, cb_false) {
     // is the response_to ID a valid thread tweet?
     if (tw_id) {
-      if (thread_tweets.hasOwnProperty(tw_id)) cb_true();
+      if (tw_threads.get_thread(tw_id)) cb_true();
       else {
         // id was not already locally cached, check if we just haven't got it yet
         twclient.get(
           "statuses/show",
           { id: tw_id, include_entities: true },
           (e, tweet, resp) => {
-            // check that it was me that tweeted this, so others
-            // can't claim the post thread tweet by tweeting a url
-            if (tweet.user.id_str == process.env.TWUID) {
-              tweet.entities.urls.forEach(tw_url => {
-                // go through each url attached and see if it's to the blog
-                if (
-                  tw_url.expanded_url.startsWith("https://www.crookm.com/journal/")
-                ) {
-                  // valid thread tweet! store it
-                  thread_tweets[tw_id] = url.parse(tw_url.expanded_url).pathname;
-                  store.put_obj(
-                    "api/obj/twitter/threads.json",
-                    thread_tweets,
-                    {
-                      ContentType: "application/json",
-                      ContentDisposition: "inline"
-                    },
-                    (err, res) => {
-                      if (err) {
-                        console.error(
-                          `[*] svcstream:twitter:stream-activity - ${err}`
-                        );
-                      } else {
-                        console.info(
-                          `[i] svcstream:twitter:stream-activity - cached new thread to spaces (ETag: ${
-                            res.ETag
-                          })`
-                        );
-                      }
-                    }
-                  );
-
-                  cb_true();
-                }
-              });
+            if (e)
+              console.error(`[*] svcstream:twitter:stream-activity - ${e}`);
+            else {
+              // check that it was me that tweeted this, so others
+              // can't claim the post thread tweet by tweeting a url
+              if (tweet.user.id_str == process.env.TWUID) {
+                tweet.entities.urls.forEach(tw_url => {
+                  // go through each url attached and see if it's to the blog
+                  if (
+                    tw_url.expanded_url.startsWith(
+                      "https://www.crookm.com/journal/"
+                    )
+                  ) {
+                    // valid thread tweet! store it
+                    tw_threads.put_thread(
+                      tw_id,
+                      url.parse(tw_url.expanded_url).pathname
+                    );
+                    cb_true();
+                  }
+                });
+              }
             }
 
             cb_false();
---
+++ util/store-tw_threads.js
@@ -0,0 +1,71 @@
+const { performance } = require("perf_hooks");
+const store = require("./store-do");
+
+let changes = 0;
+let thread_tweets = {};
+
+// refresh thread tweet store
+console.info(`[i] util:store-tw_threads - refreshing thread tweets...`);
+let thread_tweet_start = performance.now();
+store.get_obj("api/obj/twitter/threads.json", (err, data) => {
+  if (err) {
+    console.error(`[*] util:store-tw_threads- ${err}`);
+  } else {
+    let body = JSON.parse(data.Body);
+    if (body) thread_tweets = body.data;
+
+    console.info(
+      `[i] util:store-tw_threads - finished refreshing threads (${(
+        performance.now() - thread_tweet_start
+      ).toFixed(2)}ms)`
+    );
+  }
+});
+
+module.exports = {
+  get_threads: () => {
+    return thread_tweets;
+  },
+
+  get_thread: id => {
+    return thread_tweets[id];
+  },
+
+  put_thread: (id, val) => {
+    changes++;
+    thread_tweets[id] = val;
+  },
+
+  update_ep: () => {
+    if (changes > 0) {
+      let from_changes = changes;
+      console.debug(
+        `[d] util:store-tw_threads - updating ${from_changes} changes to ep`
+      );
+      store.put_obj(
+        "api/obj/twitter/threads.json",
+        thread_tweets,
+        {
+          ContentType: "application/json",
+          ContentDisposition: "inline"
+        },
+        (err, res) => {
+          if (err) console.error(`[*] util:store-tw_threads - ${err}`);
+          else {
+            console.info(
+              `[i] util:store-tw_threads - cached thread data to spaces (ETag: ${
+                res.ETag
+              })`
+            );
+
+            changes -= from_changes; // in case new changes were made since we started
+          }
+        }
+      );
+    } else console.debug(`[d] util:store-tw_threads - no updates to ep needed`);
+  }
+};
+
+// update the endpoint every 5 minutes - if there are no
+// changes, nothing will happen and no connection is made
+setInterval(module.exports.update_ep, 1000 * 60 * 5);
